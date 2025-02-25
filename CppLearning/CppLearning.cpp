1996e712e6a286d2d57da96dce687ef2ba300f8b
patriciasbar <104311967+patriciasbar@users.noreply.github.com>
2025-02-25 07:31:02 +0000

    Add project files.

---
+++ CppLearning/CppLearning.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    /*this is a comment*/
+    std::cout << "Hello World!" <<  '\n';
+    std::cout << "C++ rocks!"  << '\n';
+
+    // variables definition
+    char var1 = 'p';
+    int var2 = 2025;
+    double var3 = 9.99;
+
+    std::cout << "Value of var1 is " << var1 << '\n';
+    std::cout << "Value of var2 is " << var2 << '\n';
+    std::cout << "Value of var3 is " << var3 << '\n';
+
+    return 0;
+}
+
+
