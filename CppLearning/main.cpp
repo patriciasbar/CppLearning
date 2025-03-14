ff94e0b1a390d3520c594e08a48a5d475aa742fa
patriciasbar <104311967+patriciasbar@users.noreply.github.com>
2025-03-14 06:23:51 +0000

    add main menu

--- CppLearning/main.cpp
+++ CppLearning/main.cpp
@@ -9,15 +9,48 @@
 
 int main()
 {
-    printMessage();
-    //initialLearning();
-    //loginChecker();
-    int my_result = multiplication(2, 4);
-    std::cout << "The result is: " << my_result;
-    std::string mynickname = "Stardust";
-    customMessage(mynickname);
-    std::cout << "Int division results: " << division(10, 5) << '\n';
-    std::cout << "Double division results: " << division(12.5, 2.0) << '\n';
+    int option;
+    std::cout << "Menu: \n "
+        << " 0 - initialLearning; \n "
+        << " 1 - printMessage; \n "
+        << " 2 - customMessage; \n "
+        << " 3 - multiplication; \n "
+        << " 4 - loginChecker; \n "
+        << " 5 - division. \n ";
+    std::cout << "Enter an option... ";
+    std::cin >> option;
+    switch (option)
+    {
+    case 0:
+        initialLearning();
+        break;
+    case 1:
+        printMessage();
+        break;
+    case 2:
+    {
+        std::string mynickname = "Stardust";
+        customMessage(mynickname);
+        break;
+    }
+    case 3:
+    {
+        int my_result = multiplication(2, 4);
+        std::cout << "The result is: " << my_result;
+        break;
+    }
+    case 4: 
+        loginChecker();
+        break;
+    case 5:
+        std::cout << "Int division results: " << division(10, 5) << '\n';
+        std::cout << "Double division results: " << division(12.5, 2.0) << '\n';
+        break;
+    default:
+        std::cout << "No valid option selected.";
+        break;
+    }
+
 
 
 }
